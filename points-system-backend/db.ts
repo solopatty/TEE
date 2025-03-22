eb357ffb3d2738523b8ca83624eae26708c767e5
Paul <97986740+paulman5@users.noreply.github.com>
2025-03-22 16:16:13 +0100

    Delete points-system-backend directory

--- points-system-backend/db.ts
+++
@@ -1,9 +0,0 @@
-import postgres from "postgres"
-
-const connectionString = process.env.DATABASE_URL
-if (!connectionString) {
-  throw new Error("database url not found")
-}
-const sql = postgres(connectionString)
-
-export default sql
