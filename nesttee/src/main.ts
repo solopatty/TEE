ffc4714c6eb6dc881c8a646fac6d64cc6617e4df
0xmihirsahu <0xmihirsahu@gmail.com>
2025-03-26 05:33:07 +0530

    cors

--- nesttee/src/main.ts
+++ nesttee/src/main.ts
@@ -5,6 +5,14 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: true, // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
